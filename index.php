016112097f0ae38f38d3393d06563db61f341ad6
heyrossbutcher <heyrossbutcher@gmail.com>
2015-09-22 08:44:07 -0400

    Pushing the files up

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php //index.php is the last resort template, if no other templates match ?>
+<?php get_header(); ?>
+
+<div class="main">
+  <div class="container">
+    <div class="content">
+
+    <section class="masthead">
+    	<p>MASTHEAD</p>
+    </section><!-- End of Masthead section -->
+
+    <section class="aboutUs">
+    	<p>ABOUT</p>
+    </section><!-- End of About section -->
+
+    <section class="portfloio">
+    	<p>PORTFOLIO</p>
+    </section><!-- End of Portfilo section -->
+
+    <section class="contactUs">
+    	<p>CONTACT</p>
+    </section><!-- End of Contact section -->
+
+    		
+    </div> <!--/.content -->
+
+
+  </div> <!-- /.container -->
+</div> <!-- /.main -->
+
+<?php get_footer(); ?>
