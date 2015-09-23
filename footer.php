6e873a262975bd60fc820afc76c2c4303f004a01
heyrossbutcher <heyrossbutcher@gmail.com>
2015-09-22 23:19:48 -0400

    End of day commit

--- footer.php
+++ footer.php
@@ -1,6 +1,36 @@
 <footer>
-  <div class="wrapper">
-    <p>&copy; In Colour Capital <?php echo date('Y'); ?></p>
+  <div class="wrapper clearfix">
+	<h2>Contact</h2>
+  	<div class="credentials clearfix">
+
+  		<?php $latestPosts = new wp_query(array(
+  		  'post_type' => 'credential',//we only want about pieces
+  		  'posts_per_page' => -1
+  		)) ?>
+		
+		<?php if($latestPosts->have_posts()) while($latestPosts->have_posts()) : $latestPosts->the_post() ?>
+
+		    <?php $info = get_field('info'); ?>
+		    <?php $email = get_field('email'); ?>
+
+			<div class="creds cred_<?php the_ID(); ?>">
+				<h3><?php the_title();  //Get the Heading ?></h3>
+			    <p><?php the_field('info');  //Get the Info ?></p>
+
+			    <?php if($email) : ?>
+			    	<p><?php the_field('email');  //Get the Email ?></p>
+			    <?php endif ?>
+			</div>
+
+		<?php endwhile; // end of the loop. ?>
+
+  	</div>
+  	<div class="footer-icon">
+  		<img src="<?php bloginfo( 'template_directory' ); ?>/img/footer-icon.svg">
+  	</div>
+  	<div class="copyright">
+	    <p>&copy; In Colour Capital <?php echo date('Y'); ?></p>
+    </div>
   </div>
 </footer>
 
--- front-page.php
+++ front-page.php
@@ -10,11 +10,29 @@
 
             <section class="masthead">
                 <div class="wrapper">
+
                     <div class="animation-holder">
                     	<img src="<?php bloginfo( 'template_directory' ); ?>/img/video-placeholder.jpg" width="715" height="209" alt="In Colour Capital">
                     </div>
+
                 </div>
             </section><!-- End of Masthead section -->
+
+            <section class="intro">
+                <div class="intro">
+                    <?php $latestPosts = new wp_query(array(
+                      'post_type' => 'intro',//we only want about pieces
+                      'posts_per_page' => 1
+                    )) ?> 
+                    <div class="wrapper">
+                        <?php if($latestPosts->have_posts()) while($latestPosts->have_posts()) : $latestPosts->the_post() ?>
+
+                            <p><?php the_content(); ?></p>
+
+                        <?php endwhile; // end of the loop. ?>
+                    </div>
+                </div>
+            </section>
 
             <section class="aboutUs">
                 <?php $latestPosts = new wp_query(array(
@@ -33,8 +51,26 @@
             </section><!-- End of About section -->
 
             <section class="portfolio">
+                <?php $latestPosts = new wp_query(array(
+                  'post_type' => 'client',//we only want about pieces
+                  'posts_per_page' => -1
+                )) ?> 
                 <div class="wrapper">
-                	<p>PORTFOLIO</p>
+                    <?php if($latestPosts->have_posts()) while($latestPosts->have_posts()) : $latestPosts->the_post() ?>
+
+                        <?php $client = get_field('create_a_row');  ?>
+                        <?php $client_info = $client;  ?>
+
+                        <h2><?php the_title(); ?></h2>
+                        <?php //pre_r($client); ?>
+                        
+                        <?php $get_count = count($client_info, COUNT_RECURSIVE); ?>
+                        <?php //pre_r($get_count); ?>
+
+                        <?php //INCREMENTS OF 29 TO CHECK FOR THE NUMBER OF POSTS ?>
+
+
+                    <?php endwhile; // end of the loop. ?>
                 </div>
             </section><!-- End of Portfilo section -->
 
